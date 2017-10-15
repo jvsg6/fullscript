78cdb7966dd8a18d0346948896892d2bc119cda8
jvsg6 <jvsg6@mail.ru>
2017-10-15 23:24:32 +0300

    nakonetsto

--- run2.sh
+++ run2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 i=1
-
+a=2
 while [ $i -lt 10 ]
 do
   echo 
@@ -10,20 +10,31 @@ do
   cp trackAxis_newest.py ./0"$i"0/
   cp createVariationalSeriesGRD.py ./0"$i"0/ 
   cd 0"$i"0
+  if (("$i" >= "$a"))
+  then
+	if [ $i -eq $a ] 
+	then
+		cp dphi*.dat ../
+	fi
+	cp $(find ../ -name 'dphi*') ./0"$i"0/
+  fi
   mkdir res_"$i"0
   echo START 0"$i"0!!!
   echo $(date +"%y-%m-%d %T")
   echo START 0"$i"0 GRD
-  python createVariationalSeriesGRD.py "$PWD/SI/" 0.$i 
+  #python createVariationalSeriesGRD.py "$PWD/SI/" 0.$i 
   echo START 0"$i"0 Axis
-  python trackAxis_newest.py $PWD res_"$i"0
-  if [ '$i' -eq 2 ]; then
+  #python trackAxis_newest.py $PWD res_"$i"0
+  if [ $i -eq $a ]; then
   	cp dphi*.dat ../
   fi
+  
+  
+  cd ..
+  
+  i=$(($i + 1))
   echo DONE 0"$i"0!!!
   echo
-  i=$(($i + 1))
-  cd ..
 done
 echo 
 i=$((25))
@@ -32,6 +43,7 @@ echo $(date +"%y-%m-%d %T")
 mkdir 0"$i" 
 cp trackAxis_newest.py ./0"$i"/
 cp createVariationalSeriesGRD.py ./0"$i"/
+cp dphi*.dat ./0"$i"/
 cd 0"$i"
 mkdir res_0"$i"
 echo START 0"$i"!!!
@@ -50,6 +62,7 @@ echo $(date +"%y-%m-%d %T")
 mkdir "$i"00 
 cp trackAxis_newest.py ./"$i"00/
 cp createVariationalSeriesGRD.py ./"$i"00/
+cp dphi*.dat ./"$i"00/
 cd "$i"00
 mkdir res_"$i"00
 echo START "$i"00!!!
